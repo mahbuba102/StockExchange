2e87cfdb1855ab3512ced4d63f9436aedc08a823
mahbuba102 <automationtester102@gmail.com>
2017-04-25 10:37:59 -0700

    StockExchange Project

---
+++ src/test/java/com/weightwatchers/pagecontrollers/HomePageController.java
@@ -0,0 +1,41 @@
+package com.weightwatchers.pagecontrollers;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import com.weightwatchers.pagemodels.HomePage;
+
+import io.appium.java_client.android.AndroidDriver;
+
+public class HomePageController extends ControllerBase{
+
+	public HomePage homepage = null;
+	
+
+	public HomePageController(WebDriver driver) {
+
+		super(driver);
+		homepage = new HomePage(driver);
+	}
+
+	public void searchCompany(String stockSymbol){
+		homepage.getSearchField().sendKeys(stockSymbol);;
+	}
+	
+	public void clickSearchBtn(){
+		homepage.getSearchBtn().click();
+	}
+	public String getCompanyLabel(String companyName){
+		return homepage.getCompanyName(companyName).getText();
+	}
+	public String getStockPrice(){
+		return homepage.getStockPrice().getText();
+	}
+	public String return52WeeksYearlyHigh(){
+		return homepage.getYearlyHighElement().getText();
+	}
+	public String return52WeeksYearlyLow(){
+		return homepage.getYearlyLowElement().getText();
+	}
+	
+}
---
+++ src/test/java/com/weightwatchers/pagemodels/ModelBase.java
@@ -0,0 +1,17 @@
+package com.weightwatchers.pagemodels;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import io.appium.java_client.android.AndroidDriver;
+
+public class ModelBase {
+
+	protected WebDriver driver;
+	 public ModelBase(WebDriver driver){
+		 this.driver = driver;
+		 
+	 }
+
+
+}
